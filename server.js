15f216e672efca4d7363966f8fbbd1955a921211
helicoptersweeper <goodattackhelicoptersweeper@gmail.com>
2026-04-22 11:00:05 +0530

    Added database functionality

--- server.js
+++ server.js
@@ -1,14 +1,41 @@
+const {
+    initializeApp
+} = require('firebase/app')
+const {
+    getFirestore,
+    collection,
+    addDoc
+} = require('firebase/firestore')
+
 const express = require('express')
 const app = express()
 
 const API_KEY = 'bbf6602987ba045f06b64a1e8d428b1f'
 
+const firebaseConfig = {
+    apiKey: "AIzaSyD0ODZxE3q9UmCDGIvCLNIXbGqCjFFv_lE",
+    authDomain: "weather-app-6e36e.firebaseapp.com",
+    projectId: "weather-app-6e36e",
+    storageBucket: "weather-app-6e36e.firebasestorage.app",
+    messagingSenderId: "98556691216",
+    appId: "1:98556691216:web:496dcb84e11e28126c5b95",
+    measurementId: "G-CWP3J054FT"
+}
+
+const fbApp = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 app.set("view engine", "ejs")
 app.use(express.static("public"))
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({
+    extended: true
+}))
 
 app.get('/', (req, res) => {
-    res.render("index", { weather: null, error: null })
+    res.render("index", {
+        weather: null,
+        error: null
+    })
 })
 
 // Bug fixes:
@@ -29,7 +56,10 @@ app.post('/weather', async (req, res) => {
         const geoData = await geoResponse.json()
 
         if (!geoData || geoData.length === 0) {
-            return res.render("index", { weather: null, error: "City not found. Please try again." })
+            return res.render("index", {
+                weather: null,
+                error: "City not found. Please try again."
+            })
         }
 
         const lat = geoData[0].lat
@@ -49,11 +79,23 @@ app.post('/weather', async (req, res) => {
             wind: weaData.wind.speed
         }
 
-        res.render("index", { weather, error: null })
+        res.render("index", {
+            weather,
+            error: null
+        })
+
+        await addDoc(collection(db, "test"), {
+            city: weather.city,
+            temp: weather.temp
+        });
+        console.log("Success!");
 
     } catch (error) {
         console.error(error)
-        res.render("index", { weather: null, error: "Something went wrong. Please try again." })
+        res.render("index", {
+            weather: null,
+            error: "Something went wrong. Please try again."
+        })
     }
 })
 
